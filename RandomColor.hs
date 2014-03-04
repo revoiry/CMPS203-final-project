d99fe6264f0cd8e9c3169f5ab29dd49740af3b55
revoiry <xyuanlu@gmail.com>
2014-03-04 11:09:21 -0800

    finish random color, may need modify

--- RandomColor.hs
+++ RandomColor.hs
@@ -3,19 +3,30 @@ import Data.List
 import System.Random
 import Data.IORef
 
+--to initialize color for the block list, just call init_color parm1 parm2
+--parm1 is the length of the block list and the parm 2 is the block_list to
+--be initiate
+--
+--For example, init_color 36 block_list
+--you may need to set the global static variables for custom use
+
+
 -------global static variables-------
 
---get the number of colors and the number of randon numbers
+--set the number of colors 
 num_of_col_f :: GLfloat
 num_of_col_f = 4
 
 num_of_col_i :: Int
 num_of_col_i = 4
 
---the half of the block number
+--set the half of the block number
 half_num_of_blocks :: Int
 half_num_of_blocks = 18
 
+--the blocklist,fortest
+block_list :: [(GLfloat,GLfloat,GLfloat,Int,Int,GLfloat,Int,(GLfloat,GLfloat,GLfloat))]
+block_list = [(0.0, 0.0, 0.0, 0, 0, 0.0, 0, (0.0,0.0,0.0))| k <- [1..(2*half_num_of_blocks)] ]
 
 -------functions-------
 
@@ -41,5 +52,10 @@ n_ran_num n ran_int_list = last (take n ran_int_list)
 ran_number :: Int -> (GLfloat, GLfloat, GLfloat)
 ran_number n = last (take m (color_list ))
           where m = (n_ran_num n (ran_int_list ))
+
+--init the color for the block list
+--init_color :: t -> [t1] -> [a]
+init_color _ [] = []
+init_color n ((t0,t1,t2,t3,t4,t5,t6,_):xs) = (t0,t1,t2,t3,t4,t5,t6,(ran_number n)):(init_color (n-1) xs)
 
 
